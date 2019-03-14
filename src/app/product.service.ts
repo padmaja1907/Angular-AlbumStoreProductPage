923d83c9442bfa6deb222116ae1c41ee8bcfdfa7
Patro <padmaja.patro@accenture.com>
2019-03-14 18:57:27 +0530

    Completed the module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,13 +4,13 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  getAlbum(arg0: number): any {
-    throw new Error("Method not implemented.");
-  }
-  private _albumUrl = '../assests/album.json';
+ 
+  private _albumUrl = '../assets/album.json';
 
-  constructor(private_http:Http) {
-    return this._http.get(this._albumUrl).map((response))=> response.json());
+  constructor(private _http:Http) {}
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).map((response) => 
+    response.json());
    }
-
 }
